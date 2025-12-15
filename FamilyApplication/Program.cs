9dd68545ec7eb2eb77a7b680d11026b588cd6f54
HILURU MOHAMED <hilurumohamed456@gmail.com>
2025-12-15 21:53:33 +0530

    Enable Swagger in production

--- FamilyApplication/Program.cs
+++ FamilyApplication/Program.cs
@@ -98,11 +98,10 @@ var app = builder.Build();
 app.UseCors("AllowAngular");
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
